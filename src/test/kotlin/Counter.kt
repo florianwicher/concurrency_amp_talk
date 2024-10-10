77d6e9e8ea963d615e61fa44a37775cd35406ee0
Florian Wicher <florian.wicher@rise-world.com>
2024-10-10 10:23:24 +0200

    pessimistic vs optimistic counter

--- src/test/kotlin/Counter.kt
+++ src/test/kotlin/Counter.kt
@@ -1,10 +1,23 @@
 import org.junit.jupiter.api.Test
+import java.util.concurrent.atomic.AtomicInteger
 
 class Counter {
     @Volatile
     var count = 0
     fun increment() {
         count++
+    }
+}
+
+class OptimisticCounter {
+    private var count = AtomicInteger(0)
+
+    fun increment() {
+        while (true) {
+            val currentCount = count.get()
+            val newCount = currentCount + 1
+            if (count.compareAndSet(currentCount, newCount)) return
+        }
     }
 }
 
@@ -26,5 +39,27 @@ class CounterTest {
               |that's ${100 * (timesIncrementWasCalled - actualCount) / timesIncrementWasCalled}%""".trimMargin()
         )
         assert(actualCount == timesIncrementWasCalled)
+    }
+
+    @Test
+    fun `pessimistic vs optimistic counter`() {
+        val pessimisticCounter = Counter()
+        val optimisticCounter = OptimisticCounter()
+
+        val threadCount = 1
+        val iterations = 100_000
+
+        val pessimisticBlaster = blast(threadCount, iterations) {
+            pessimisticCounter.increment()
+        }
+
+        val optimisticBlaster = blast(threadCount, iterations) {
+            optimisticCounter.increment()
+        }
+
+        println(
+            """Regular counter: ${pessimisticBlaster.duration}
+              |Optimistic counter: ${optimisticBlaster.duration}""".trimMargin()
+        )
     }
 }
